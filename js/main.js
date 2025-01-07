08f7e5465e62bf624777b281563b6fe8281efd2f
Ko-gwang-ho <xonic1104@gmail.com>
2025-01-07 20:47:44 +0900

    안드로이드 느림 현상 테스트
    main.js 수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // 라이브러리
 import './scroll-timeline.js';
-// import './lenis.js';
+import './lenis.js';
 // 사이트 내
 import './dark_mode.js';
-// import './scroll.js';
+import './scroll.js';
 import './main_object.js';
